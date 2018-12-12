8ed48426fc562b0b0796a0934a0db64d41133f7c
chitty <rychitty@icloud.com>
2018-12-12 15:16:16 -0600

    added fs functionality, does not work yet

--- liri.js
+++ liri.js
@@ -1,29 +1,97 @@
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var moment = require('moment');
+var fs = require('fs');
 var nodeArgs = process.argv;
 var movieName = "";
 var movieDefault = 'Mr. Nobody';
 var artistName = '';
 var defaultArtist ='Panic! At The Disco';
+var songName = '';
+var songDefault = 'the sign';
 
 
 require("dotenv").config();
 
 var keys = require('./keys.js');
 
-var spotifyClient = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 // console.log(spotifyClient);
 
-    
-// https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=fbd9243030e50e0475cd78765e9cb68a
-    
-// https://rest.bandsintown.com/artists/Skrillex/events?app_id=fbd9243030e50e0475cd78765e9cb68a
+//DO-WHAT-IT-SAYS
+if(process.argv[2] === 'do-what-it-says' && process.argv[3] != undefined){
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable "data"
+fs.readFile("random.txt", "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  // Break the string down by comma separation and store the contents into the output array.
+  // var output = data.split(",");
+  var output = data.split(',');
+
+  // Loop Through the newly created output array
+  for (var i = 0; i < output.length; i++) {
+
+    // Print each element (item) of the array/
+    console.log(output[i]);
+  }
+});
+}
+
+//SPOTIFY-THIS-SONG
+if(process.argv[2] === 'spotify-this-song' && process.argv[3] !== undefined){
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      songName = songName + "+" + nodeArgs[i];
+    }
+    else {
+      songName += nodeArgs[i];
+    }
+  }
+
+  spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    //song name for debugging
+    // console.log(songName);
+    //parses all data for debugging
+    // console.log(JSON.stringify(data, null, 2)); 
+//artist name
+console.log('[Artist] : ' + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+//songs name
+console.log('[Song] : ' + JSON.stringify(data.tracks.items[0].name, null, 2)); 
+//external url
+console.log('[URL] : ' + JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2)); 
+//album name
+console.log('[Album] : ' + JSON.stringify(data.tracks.items[0].album.name, null, 2)); 
 
 
+  });
 
+} else if (process.argv[2] === "spotify-this-song" && process.argv[3] === undefined){
+  spotify.search({ type: 'track', query: songDefault, limit: 5 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
 
+        // console.log(JSON.stringify(data, null, 2)); 
+
+    //artist name
+console.log('[Artist] : ' + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+//songs name
+console.log('[Song] : ' + JSON.stringify(data.tracks.items[0].name, null, 2)); 
+//external url
+console.log('[URL] : ' + JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2)); 
+//album name
+console.log('[Album] : ' + JSON.stringify(data.tracks.items[0].album.name, null, 2)); 
+  });
+}
+
+//CONCERT-THIS
 if(process.argv[2] === "concert-this" && process.argv[3] !== undefined) {
   //handling spaces in the request if needed
   for (var i = 3; i < nodeArgs.length; i++) {
