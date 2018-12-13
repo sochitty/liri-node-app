374d8430ca5a2733bf64da2f2772bd806c4afccb
chitty <rychitty@icloud.com>
2018-12-12 21:08:39 -0600

    got do-what-it-says to work

--- liri.js
+++ liri.js
@@ -20,26 +20,71 @@ var spotify = new Spotify(keys.spotify);
 // console.log(spotifyClient);
 
 //DO-WHAT-IT-SAYS
-if(process.argv[2] === 'do-what-it-says' && process.argv[3] != undefined){
+if(process.argv[2] === 'do-what-it-says' && process.argv[3] === undefined){
 // Running the readFile module that's inside of fs.
 // Stores the read information into the variable "data"
+var content;
 fs.readFile("random.txt", "utf8", function(err, data) {
   if (err) {
     return console.log(err);
   }
-
   // Break the string down by comma separation and store the contents into the output array.
   // var output = data.split(",");
-  var output = data.split(',');
+  content = data.split(",");
+  
+  if(content[0] === 'spotify-this-song'){
+    spotify.search({ type: 'track', query: content[1], limit: 1 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      //song name for debugging
+      // console.log(songName);
+      //parses all data for debugging
+      // console.log(JSON.stringify(data, null, 2)); 
+  //artist name
+  console.log('[Artist] : ' + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+  //songs name
+  console.log('[Song] : ' + JSON.stringify(data.tracks.items[0].name, null, 2)); 
+  //external url
+  console.log('[URL] : ' + JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2)); 
+  //album name
+  console.log('[Album] : ' + JSON.stringify(data.tracks.items[0].album.name, null, 2)); 
+  
+  
+    });
+  } else if(content[0] === 'movie-this'){
+    for (var i = 3; i < nodeArgs.length; i++) {
+      if (i > 3 && i < nodeArgs.length) {
+        movieName = movieName + "+" + nodeArgs[i];
+      }
+      else {
+        movieName += nodeArgs[i];
+      }
+    }
+    
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + content[1] + "&y=&plot=short&apikey=127a5741";
+    
+    // This line is just to help us debug against the actual URL.
+    // console.log(queryUrl);
+    
+    axios.get(queryUrl).then(
+      function(response) {
+        console.log("[Title]:" + response.data.Title);
+        console.log("[Year]:" + response.data.Year);
+        console.log("[IMDB Rating]:" + response.data.imdbRating);
+        console.log("[Rotten Tomatoes Rating]:" + response.data.Ratings[1].Value);
+        console.log("[Produced In]:" + response.data.Country);
+        console.log("[Language]:" + response.data.Language);
+        console.log("[Plot]:" + response.data.Plot);
+        console.log("[Actors]:" + response.data.Actors);
 
-  // Loop Through the newly created output array
-  for (var i = 0; i < output.length; i++) {
-
-    // Print each element (item) of the array/
-    console.log(output[i]);
+      }
+    );
   }
 });
 }
+
 
 //SPOTIFY-THIS-SONG
 if(process.argv[2] === 'spotify-this-song' && process.argv[3] !== undefined){
@@ -78,7 +123,7 @@ console.log('[Album] : ' + JSON.stringify(data.tracks.items[0].album.name, null,
       return console.log('Error occurred: ' + err);
     }
 
-        // console.log(JSON.stringify(data, null, 2)); 
+       console.log("the song is: " + songDefault); 
 
     //artist name
 console.log('[Artist] : ' + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
@@ -161,7 +206,7 @@ if(process.argv[2] === "concert-this" && process.argv[3] !== undefined) {
 
 
 
-
+//MOVIE-THIS
 
 
     //if the arguments are movie related and actually DO have a request from user then run this
@@ -180,7 +225,7 @@ if(process.argv[2] === "concert-this" && process.argv[3] !== undefined) {
           var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=127a5741";
           
           // This line is just to help us debug against the actual URL.
-          console.log(queryUrl);
+          // console.log(queryUrl);
           
           axios.get(queryUrl).then(
             function(response) {
@@ -218,4 +263,4 @@ if(process.argv[2] === "concert-this" && process.argv[3] !== undefined) {
 
           }
         );
-    }
+    } 
